8be50fea5de8c43d1baba8a8de2bb91f35007f9c
Elyees Tatua <elyeestatua@gmail.com>
2024-12-06 00:11:15 +0300

    feat : updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,4 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  theme: {  },
